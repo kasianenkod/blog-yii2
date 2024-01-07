f19857a31a4e54aac2a4e9cbcfcb4509d5425241
Kasianenko Danil <kasianenkod00@gmail.com>
2024-01-07 13:21:58 +0200

    add controllers and views for admin

--- config/web.php
+++ config/web.php
@@ -54,6 +54,13 @@ $config = [
         ],
 
     ],
+
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
+
     'params' => $params,
 ];
 
